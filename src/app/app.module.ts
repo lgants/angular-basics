6e5f3cd6d1f8666329097bbd95e772dad71ddcf2
lgants <lgants@gmail.com>
2017-07-15 13:21:13 -0500

    added basic server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+// angular will not scan files to find components; those components must be manually added here
+// this grabs the ts file
+import { ServerComponent } from './server/server.component';
 
+// bootstrap is the component the app should be aware of when the base application starts
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +20,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
+// AppComponent is TypeScript class; transformed into something with NgModule decorator
 export class AppModule { }
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Component is app component decorator
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: []
+})
+export class ServerComponent {
+
+}
